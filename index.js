370ece78c219549f50462ab170e95aec9e77aff5
Schwifty <schwifty@schwifty.space>
2017-01-18 22:35:32 +0100

    Listen on port 3000

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/www/index.html');
 });
 
-app.listen(80, function () {
-  console.log('listening on port 80');
+app.listen(3000, function () {
+  console.log('listening on port 3000');
 })

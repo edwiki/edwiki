6c982779c05fd7888dc17d34791eb81859651e1b
Schwifty <schwifty@schwifty.space>
2017-01-18 22:35:55 +0100

    Rename www to public

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('www'));
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/www/index.html');
+	res.sendFile(__dirname + 'public/index.html');
 });
 
 app.listen(3000, function () {
